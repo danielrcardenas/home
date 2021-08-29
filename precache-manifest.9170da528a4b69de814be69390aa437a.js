69e693f6667a7fb3938756ccfbbd1cad8fa407a4
Daniel R Cardenas <danielrcardenas@gmail.com>
2021-08-29 09:06:28 -0400

    Updates

---
+++ precache-manifest.9170da528a4b69de814be69390aa437a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf604a3a1df9b1eded9174b6332aeae",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "f0ebcc9085fb4d8d49d9",
+    "url": "/home/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5640430afb9ef7eca762",
+    "url": "/home/static/css/main.c18ca85e.chunk.css"
+  },
+  {
+    "revision": "f0ebcc9085fb4d8d49d9",
+    "url": "/home/static/js/2.53ed34d2.chunk.js"
+  },
+  {
+    "revision": "cf5689887d395d116a38769473339df4",
+    "url": "/home/static/js/2.53ed34d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5640430afb9ef7eca762",
+    "url": "/home/static/js/main.149b753e.chunk.js"
+  },
+  {
+    "revision": "a543b8b6541bc0a6d9fc",
+    "url": "/home/static/js/runtime-main.8bd7cfab.js"
+  },
+  {
+    "revision": "b19b0b4bebfda3983a498df5981be2a9",
+    "url": "/home/static/media/resume.b19b0b4b.pdf"
+  }
+]);
